3ad0051258c9ab1f08a5f7f361bc3c6f49d3bb92
hoang11102002 <thanhhoang11102002@gmail.com>
2025-01-14 11:02:27 +0700

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,68 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import pluginJs from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
+import pluginTailwindCSS from "eslint-plugin-tailwindcss";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      globals: globals.node,
     },
   },
-)
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      tailwindcss: pluginTailwindCSS,
+      "unused-imports": pluginUnusedImports,
+      import: pluginImport,
+    },
+    rules: {
+      "tailwindcss/classnames-order": "error",
+      "tailwindcss/no-custom-classname": "off",
+
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "react/react-in-jsx-scope": "off",
+
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "clsx", "cva"],
+        config: "./tailwind.config.js",
+        whitelist: ["ph", "ph-"],
+      },
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className='flex  items-center justify-center text-xs text-black'>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="">
         Click on the Vite and React logos to learn more
       </p>
     </>
